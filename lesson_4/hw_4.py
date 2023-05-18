8935c4996ce5255b18c94a416c8fcaf4f95c7b5d
marin <marina.kirnos91@gmail.com>
2023-05-18 13:50:00 +0300

    git

--- lesson_4/hw_4.py
+++ lesson_4/hw_4.py
@@ -1,5 +1,4 @@
 # var 3
-# import string
 words_welcome = ('привіт', 'хай', 'доброго дня', 'доброго вечора')
 words_make = ('як справи', 'що робиш', 'чим займаєшся')
 words_film = ('фільм', 'кінотеатр', 'серіал')
@@ -12,15 +11,15 @@ while not input_bay:
     for word in words_welcome:
         if word in input_string:
             print('Доброго вечора, я бот з України!')
-            # find_word = True
+            find_word = True
     for word in words_make:
         if word in input_string:
             print('Вчусь програмувати на Python!')
             find_word = True
     for word in words_film:
         if word in input_string:
-            print('Соррі що втручаюсь, не знаю про що йдеться мова, але подивіться серіал/фільм -'
-                  ' Друзі, він просто бомба!')
+            print('Соррі що втручаюсь, не знаю про що йдеться мова, але подивіться серіал/фільм - '
+                  'Друзі, він просто бомба!')
             find_word = True
     for word in words_bay:
         if word in input_string:
